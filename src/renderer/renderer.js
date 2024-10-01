b1e2622ad484e3d66d0ef463978b204de67cef27
Felipe Augusto <80834084@telefonicati.onmicrosoft.com>
2024-10-01 16:35:50 -0300

    feat: add JAR file and CRX folder selection screen

---
+++ src/renderer/renderer.js
@@ -0,0 +1,16 @@
+// src/renderer/renderer.js
+const { ipcRenderer } = require('electron');
+
+// Seleção do arquivo JAR
+document.getElementById('selectJarBtn').addEventListener('click', async () => {
+    const jarPath = await ipcRenderer.invoke('select-jar-file');
+    document.getElementById('jarPath').innerText = `Caminho do arquivo JAR: ${jarPath || 'Nenhum arquivo selecionado'}`;
+});
+
+// Seleção da pasta CRX
+document.getElementById('selectCrxBtn').addEventListener('click', async () => {
+    const crxPath = await ipcRenderer.invoke('select-crx-folder');
+    document.getElementById('crxPath').innerText = `Caminho da pasta CRX: ${crxPath || 'Nenhuma pasta selecionada'}`;
+});
+
+
